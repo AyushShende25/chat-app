bc062187bfdc2a57b309ac36397b7a20a1129de3
AyushShende25 <ayushshende9@gmail.com>
2026-01-25 23:18:19 +0530

    refactor(register-endpoint): handle unique-constraint in try-catch

--- services/auth/src/services/register.service.ts
+++ services/auth/src/services/register.service.ts
@@ -1,6 +1,6 @@
 import { ConflictError } from "@chat-app/errors";
 import { AUTH_TOPICS } from "@chat-app/events";
-import { eq } from "drizzle-orm";
+import { DrizzleQueryError } from "drizzle-orm";
 import { db } from "../db";
 import { accounts } from "../db/schema/account";
 import { emailVerificationTokens } from "../db/schema/email-verification";
@@ -10,45 +10,47 @@ import { hashPassword } from "../utils/password";
 
 export const registerAccount = async (registerInput: RegisterInput) => {
 	const { email, password } = registerInput;
-	const existingAcc = await db
-		.select()
-		.from(accounts)
-		.where(eq(accounts.email, email));
-	console.log(existingAcc, "e");
-
-	if (existingAcc.length > 0) {
-		throw new ConflictError("Account already exists");
-	}
 
 	const passwordHash = await hashPassword(password);
 
-	const [account] = await db
-		.insert(accounts)
-		.values({
-			email,
-			passwordHash,
-		})
-		.returning();
-	if (!account) {
-		throw new Error("Failed to create account");
+	try {
+		const [account] = await db
+			.insert(accounts)
+			.values({
+				email,
+				passwordHash,
+			})
+			.returning();
+
+		if (!account) {
+			throw new Error("Failed to create account");
+		}
+
+		const verificationToken = crypto.randomUUID();
+
+		await db.insert(emailVerificationTokens).values({
+			id: verificationToken,
+			accountId: account.id,
+			expiresAt: new Date(Date.now() + 1000 * 60 * 60),
+		});
+
+		await publishAuthEvent({
+			topic: AUTH_TOPICS.ACCOUNT_CREATED,
+			key: account.id,
+			value: { accountId: account.id, email: account.email, verificationToken },
+		});
+
+		return {
+			accountId: account.id,
+			verificationToken,
+		};
+	} catch (error) {
+		if (error instanceof DrizzleQueryError) {
+			const pgError = error.cause as { code: string } | undefined;
+			if (pgError?.code === "23505") {
+				throw new ConflictError("Account already exists");
+			}
+		}
+		throw error;
 	}
-
-	const verificationToken = crypto.randomUUID();
-
-	await db.insert(emailVerificationTokens).values({
-		id: verificationToken,
-		accountId: account.id,
-		expiresAt: new Date(Date.now() + 1000 * 60 * 60),
-	});
-
-	await publishAuthEvent({
-		topic: AUTH_TOPICS.ACCOUNT_CREATED,
-		key: account.id,
-		value: { accountId: account.id, email: account.email, verificationToken },
-	});
-
-	return {
-		accountId: account.id,
-		verificationToken,
-	};
 };
