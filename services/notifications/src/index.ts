29280031fe1604108e5aeaa836c7baa1a40f92d0
AyushShende25 <ayushshende9@gmail.com>
2026-01-25 23:20:25 +0530

    fix: add startAuthEventsConsumer to bootstrap

--- services/notifications/src/index.ts
+++ services/notifications/src/index.ts
@@ -1,8 +1,10 @@
+import { startAuthEventsConsumer } from "./consumers/auth";
 import { initKafkaConsumer } from "./lib/kafka/consumer";
 import { logger } from "./lib/logger";
 
 const bootstrap = async () => {
 	await initKafkaConsumer();
+	await startAuthEventsConsumer();
 	logger.info("Notifications service started");
 };
 
